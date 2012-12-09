137631127943d873e7d701d57f3e02d049fa4a32
Mike Pavone <pavone@retrodev.com>
2012-12-08 22:07:25 -0800

    Add support for simple resolution scaling

--- stateview.c
+++ stateview.c
@@ -14,11 +14,21 @@ int main(int argc, char ** argv)
 		fprintf(stderr, "Failed to open %s\n", argv[1]);
 		exit(1);
 	}
+	int width = 320;
+	int height = 240;
+	if (argc > 2) {
+		width = atoi(argv[2]);
+		if (argc > 3) {
+			height = atoi(argv[3]);
+		} else {
+			height = (width/320) * 240;
+		}
+	}
 	vdp_context context;
 	init_vdp_context(&context);
 	vdp_load_savestate(&context, state_file);
 	vdp_run_to_vblank(&context);
-    render_init();
+    render_init(width, height);
     render_context(&context);
     render_wait_quit();
     return 0;
