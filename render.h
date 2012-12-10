aa00a0b53cd984d37d454fd74360216dea966d2a
Mike Pavone <pavone@retrodev.com>
2012-12-09 18:40:45 -0800

    Add debug render mode and fix vertical flip bit for bg tiles

--- render.h
+++ render.h
@@ -4,7 +4,7 @@
 #include "vdp.h"
 void render_init(int width, int height);
 void render_context(vdp_context * context);
-void render_wait_quit();
+void render_wait_quit(vdp_context * context);
 
 #endif //RENDER_SDL_H_
 
