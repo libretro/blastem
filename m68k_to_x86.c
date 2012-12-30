dc0d0a7991998452ed056cc50db4e149a0b4cf2a
Mike Pavone <pavone@retrodev.com>
2012-12-29 23:40:30 -0800

    Fix swap

--- m68k_to_x86.c
+++ m68k_to_x86.c
@@ -2564,9 +2564,9 @@ uint8_t * translate_m68k(uint8_t * dst, m68kinst * inst, x86_68k_options * opts)
 	case M68K_SWAP:
 		dst = cycles(dst, BUS);
 		if (src_op.mode == MODE_REG_DIRECT) {
-			dst = rol_ir(dst, 16, src_op.base, inst->extra.size);
+			dst = rol_ir(dst, 16, src_op.base, SZ_D);
 		} else{
-			dst = rol_irdisp8(dst, 16, src_op.base, src_op.disp, inst->extra.size);
+			dst = rol_irdisp8(dst, 16, src_op.base, src_op.disp, SZ_D);
 		}
 		dst = mov_ir(dst, 0, FLAG_C, SZ_B);
 		dst = setcc_r(dst, CC_Z, FLAG_Z);
