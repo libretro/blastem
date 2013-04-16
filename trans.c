5b8b2105a5ebee2f9d272c06ba129c7f1fbb8922
Mike Pavone <pavone@retrodev.com>
2013-04-16 09:38:30 -0700

    Fixed up trans for changes to translate_m68k_stream, but still need to deal with missing callbacks.

--- trans.c
+++ trans.c
@@ -9,9 +9,6 @@ int main(int argc, char ** argv)
 	long filesize;
 	unsigned short *filebuf;
 	char disbuf[1024];
-	size_t size = 1024 * 1024;
-	uint8_t * transbuf = alloc_code(&size);
-	uint8_t *trans_cur, *end;
 	unsigned short * cur;
 	x86_68k_options opts;
 	m68k_context context;
@@ -33,7 +30,9 @@ int main(int argc, char ** argv)
 	context.target_cycle = 0x7FFFFFFF;
 	//work RAM
 	context.mem_pointers[1] = malloc(64 * 1024);
-	translate_m68k_stream(transbuf, transbuf + size, 0, &context);
+	uint32_t address;
+	address = filebuf[2] << 16 | filebuf[3];
+	translate_m68k_stream(address, &context);
 	m68k_reset(&context);
 	return 0;
 }
