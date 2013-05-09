cac23fc79102554d581457b94b084275588cfc22
Mike Pavone <pavone@retrodev.com>
2013-05-08 23:20:41 -0700

    Set PV flag based on parity, not overflow for XOR

--- z80_to_x86.c
+++ z80_to_x86.c
@@ -736,7 +736,7 @@ uint8_t * translate_z80inst(z80inst * inst, uint8_t * dst, z80_context * context
 		dst = mov_irdisp8(dst, 0, CONTEXT, zf_off(ZF_N), SZ_B);
 		//TODO: Implement half-carry flag
 		if (z80_size(inst) == SZ_B) {
-			dst = setcc_rdisp8(dst, CC_O, CONTEXT, zf_off(ZF_PV));
+			dst = setcc_rdisp8(dst, CC_P, CONTEXT, zf_off(ZF_PV));
 			dst = setcc_rdisp8(dst, CC_Z, CONTEXT, zf_off(ZF_Z));
 			dst = setcc_rdisp8(dst, CC_S, CONTEXT, zf_off(ZF_S));
 		}
