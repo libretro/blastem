7be628de2d8bf36b66443ec37fc9d355fad56635
Mike Pavone <pavone@retrodev.com>
2013-09-10 23:31:08 -0700

    Added copyright notice to source files and added GPL license text in COPYING

--- ztestgen.c
+++ ztestgen.c
@@ -1,3 +1,8 @@
+/*
+ Copyright 2013 Michael Pavone
+ This file is part of BlastEm. 
+ BlastEm is free software distributed under the terms of the GNU General Public License version 3 or greater. See COPYING for full license text.
+*/
 #include "z80inst.h"
 #include <stdlib.h>
 #include <string.h>
