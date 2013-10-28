ddd6db30d1603785de9381224257c7ffdc3ad670
Mike Pavone <pavone@retrodev.com>
2013-10-27 21:41:21 -0700

    Support interlace mode with OpenGL backend

--- default.f.glsl
+++ default.f.glsl
@@ -6,5 +6,9 @@ varying vec2 texcoord;
 
 void main()
 {
-	gl_FragColor = texture2D(textures[0], texcoord);
+	gl_FragColor = mix(
+		texture2D(textures[0], texcoord),
+		texture2D(textures[1], vec2(texcoord.x, texcoord.y - 1.0/512.0)),
+		sin((texcoord.y * 512.0 - 0.75) * 3.14159265359) / 2.0 + 0.5
+	);
 }
