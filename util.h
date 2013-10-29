f10f8425a83a4aef39631a6b4e00562f79227fca
Mike Pavone <pavone@retrodev.com>
2013-10-28 21:48:46 -0700

    Extract function to determine executable directory from load_config so it can be used elsewhere

--- util.h
+++ util.h
@@ -13,6 +13,13 @@ char * alloc_concat_m(int num_parts, char ** parts);
 long file_size(FILE * f);
 //Strips whitespace and non-printable characters from the beginning and end of a string
 char * strip_ws(char * text);
+//Inserts a null after the first word, returns a pointer to the second word
 char * split_keyval(char * text);
+//Should be called by main with the value of argv[0] for use by get_exe_dir
+void set_exe_str(char * str);
+//Returns the directory the executable is in
+char * get_exe_dir();
+//Returns the contents of a symlink in a newly allocated string
+char * readlink_alloc(char * path);
 
 #endif //UTIL_H_
