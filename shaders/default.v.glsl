b009c3faf7c6531a883a25d5d425bea157a0d37a
Mike Pavone <pavone@retrodev.com>
2013-10-29 19:09:19 -0700

    Preserve aspect ratio unless config file says otherwise

--- shaders/default.v.glsl
+++ shaders/default.v.glsl
@@ -6,5 +6,5 @@ varying vec2 texcoord;
 void main()
 {
 	gl_Position = vec4(pos, 0.0, 1.0);
-	texcoord = pos * vec2(320.0/1024.0, 240.0/-512.0) + vec2(320.0/1024.0, 240.0/512.0);
+	texcoord = sign(pos) * vec2(320.0/1024.0, 240.0/-512.0) + vec2(320.0/1024.0, 240.0/512.0);
 }
