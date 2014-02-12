5f77a699631ee74a6c1876ad2727e876b923e202
Mike Pavone <pavone@retrodev.com>
2014-02-11 21:53:31 -0800

    Move debugging code outside of main source file

---
+++ debug.h
@@ -0,0 +1,26 @@
+#ifndef DEBUG_H_
+#define DEBUG_H_
+
+#include <stdint.h>
+#include "m68k_to_x86.h"
+
+typedef struct disp_def {
+	struct disp_def * next;
+	char *            param;
+	uint32_t          index;
+	char              format_char;
+} disp_def;
+
+typedef struct bp_def {
+	struct bp_def * next;
+	uint32_t address;
+	uint32_t index;
+} bp_def;
+
+bp_def ** find_breakpoint(bp_def ** cur, uint32_t address);
+bp_def ** find_breakpoint_idx(bp_def ** cur, uint32_t index);
+void add_display(disp_def ** head, uint32_t *index, char format_char, char * param);
+void remove_display(disp_def ** head, uint32_t index);
+m68k_context * debugger(m68k_context * context, uint32_t address);
+
+#endif //DEBUG_H_
