99ddd5dce6efa066d671beb5a8f01a7a5f80d0d0
Michael Pavone <pavone@retrodev.com>
2015-06-28 10:21:51 -0700

    Use MAP_32BIT on Linux since my hint seems to be ignored

--- mem.c
+++ mem.c
@@ -14,13 +14,17 @@
 #define MAP_ANONYMOUS MAP_ANON
 #endif
 
+#ifndef MAP_32BIT
+#define MAP_32BIT 0
+#endif
+
 void * alloc_code(size_t *size)
 {
 	//start at the 1GB mark to allow plenty of room for sbrk based malloc implementations
 	//while still keeping well within 32-bit displacement range for calling code compiled into the executable
 	static uint8_t *next = (uint8_t *)0x40000000;
 	*size += PAGE_SIZE - (*size & (PAGE_SIZE - 1));
-	uint8_t *ret = mmap(NULL, *size, PROT_EXEC | PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	uint8_t *ret = mmap(NULL, *size, PROT_EXEC | PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_32BIT, -1, 0);
 	next = ret + *size;
 	return ret;
 }
