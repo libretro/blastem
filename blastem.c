ff238c672d2f623363169f7ea6a3f292bbb24543
Michael Pavone <pavone@retrodev.com>
2015-12-08 19:33:58 -0800

    Allow IO device config to be overriden by ROM DB

--- blastem.c
+++ blastem.c
@@ -860,7 +860,7 @@ genesis_context *alloc_init_genesis(rom_info *rom, int fps, uint32_t ym_opts)
 	gen->cart = cart;
 	gen->work_ram = ram;
 	gen->zram = z80_ram;
-	setup_io_devices(config, gen->ports);
+	setup_io_devices(config, rom, gen->ports);
 
 	gen->save_type = rom->save_type;
 	gen->save_type = rom->save_type;
