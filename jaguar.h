3bda38c2547f87a25635a886604e8b597bae5491
Michael Pavone <pavone@retrodev.com>
2016-10-07 18:27:38 -0700

    Added placeholder code for video output hardware/object processor

--- jaguar.h
+++ jaguar.h
@@ -1,13 +1,15 @@
 #ifndef JAGUAR_H_
 #define JAGUAR_H_
 
-#define DRAM_WORDS (4*1024*1024)
-#define LINEBUFFER_WORDS 720
+#define DRAM_WORDS (1*1024*1024)
 #define GPU_RAM_BYTES 4096
 #define DSP_RAM_BYTES 8192
 
+#include "jag_video.h"
+
 typedef struct {
 	m68k_context *m68k;
+	jag_video    *video;
 	uint16_t     *bios;
 	uint16_t     *cart;
 	uint32_t     bios_size;
@@ -20,11 +22,6 @@ typedef struct {
 	uint16_t     dram[DRAM_WORDS];
 	uint32_t     gpu_local[GPU_RAM_BYTES / sizeof(uint32_t)];
 	uint32_t     dsp_local[DSP_RAM_BYTES / sizeof(uint32_t)];
-	uint16_t     clut[256];
-	uint16_t     line_buffer_a[LINEBUFFER_WORDS];
-	uint16_t     line_buffer_b[LINEBUFFER_WORDS];
-	uint16_t     *write_line_buffer;
-	uint16_t     *read_line_buffer;
 	
 	uint8_t      memcon_written;
 } jaguar_context;
