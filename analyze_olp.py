12f36b093068d9b9398d608ef10d5467817bd250
Michael Pavone <pavone@retrodev.com>
2017-01-03 21:18:52 -0800

    Add support for SMS controllers

--- analyze_olp.py
+++ analyze_olp.py
@@ -18,6 +18,16 @@ def detect_high(sample, bit):
 def detect_low(sample, bit):
 	mask = 1 << bit
 	return not sample & mask
+	
+def get_value(sample, bits):
+	value = 0
+	for i in xrange(0, len(bits)):
+		bit = bits[i]
+		value |= (sample >> bit & 1) << i
+	return value
+	
+def swizzle_mode4(row, col):
+	return (col & 1) | (row << 1) | (col << 8 & 0xFE00)
 
 def analyze_delays(chanmap, datafile):
 	if 'M68K_CLK' in chanmap:
@@ -83,7 +93,61 @@ def analyze_refresh(chanmap, datafile):
 						print 'RAM refresh at', clks, 'delta since last:', clks-prevRefresh
 					prevRefresh = clks
 			last = sample
-				
+			
+			
+table_start = 0x3800
+table_end = table_start + 0x600
+sat_start = 0x3F00 #0x3E00
+sat_xname = sat_start + 0x80
+sat_end = sat_start + 0x100
+
+
+def analyze_vram(chanmap, datafile):
+	address_bits = [chanmap['AD{0}'.format(i)] for i in xrange(0, 8)]
+	ras = chanmap['!RAS']
+	cas = chanmap['!CAS']
+	hsync = chanmap['!HSYNC']
+	state = 'begin'
+	last = False
+	for line in datafile.readlines():
+		line = line.strip()
+		if line and not line.startswith(';'):
+			sample,_,num = line.partition('@')
+			sample = int(sample, 16)
+			if not (last is False):
+				if detect_fall(last, sample, hsync):
+					print 'HSYNC low @ {0}'.format(num)
+				elif detect_rise(last, sample, hsync):
+					print 'HSYNC high @ {0}'.format(num)
+				if state == 'begin':
+					if detect_fall(last, sample, ras):
+						state = 'ras'
+						row = get_value(sample, address_bits)
+					elif detect_fall(last, sample, cas):
+						state = 'cas'
+				elif state == 'ras':
+					if detect_fall(last, sample, cas):
+						col = get_value(sample, address_bits)
+						address = swizzle_mode4(row, col)
+						
+						if address < table_end and address >= table_start:
+							offset = (address - table_start)/2
+							desc = 'Map Row {0} Col {1}'.format(offset / 32, offset & 31)
+						elif address >= sat_start and address < sat_xname:
+							offset = address - sat_start
+							desc = 'Sprite {0} Y Read'.format(offset)
+						elif address >= sat_xname and address < sat_end:
+							offset = address - sat_xname
+							desc = 'Sprite {0} X/Name Read'.format(offset / 2)
+						else:
+							desc = 'Tile {0} Row {1}'.format(address / 32, ((address / 4) & 7) + (0.5 if address & 2 else 0))
+						print '{0:02X}:{1:02X} - {2:04X} @ {3} - {4}'.format(row, col, address, num, desc)
+						state = 'begin'
+				elif state == 'cas':
+					if detect_fall(last, sample, ras):
+						print 'refresh @ {0}'.format(num)
+						state = 'begin'
+			last = sample
 
 def main(args):
 	if len(args) < 2:
@@ -98,7 +162,8 @@ def main(args):
 	for i in xrange(0, len(channels)):
 		chanmap[channels[i]] = i
 	datafile = olpfile.open('data.ols')
-	analyze_delays(chanmap, datafile)
+	#analyze_delays(chanmap, datafile)
+	analyze_vram(chanmap, datafile)
 	datafile.close()
 	#datafile = olpfile.open('data.ols')
 	#analyze_refresh(chanmap, datafile)
