86e396a87cb6c47439ad7e696028a7a31869f5b3
Michael Pavone <pavone@retrodev.com>
2017-01-22 16:13:02 -0800

    Consting up some parameters to ternary tree functions

--- tern.h
+++ tern.h
@@ -27,17 +27,17 @@ typedef struct tern_node {
 
 typedef void (*iter_fun)(char *key, tern_val val, void *data);
 
-tern_node * tern_insert(tern_node * head, char * key, tern_val value);
-int tern_find(tern_node * head, char * key, tern_val *ret);
-tern_node * tern_find_prefix(tern_node * head, char * key);
-intptr_t tern_find_int(tern_node * head, char * key, intptr_t def);
-tern_node * tern_insert_int(tern_node * head, char * key, intptr_t value);
-void * tern_find_ptr_default(tern_node * head, char * key, void * def);
-void * tern_find_ptr(tern_node * head, char * key);
-tern_val tern_find_path_default(tern_node *head, char *key, tern_val def);
-tern_val tern_find_path(tern_node *head, char *key);
-tern_node * tern_insert_ptr(tern_node * head, char * key, void * value);
-tern_node * tern_insert_node(tern_node *head, char *key, tern_node *value);
+tern_node * tern_insert(tern_node * head, char const * key, tern_val value);
+int tern_find(tern_node * head, char const * key, tern_val *ret);
+tern_node * tern_find_prefix(tern_node * head, char const * key);
+intptr_t tern_find_int(tern_node * head, char const * key, intptr_t def);
+tern_node * tern_insert_int(tern_node * head, char const * key, intptr_t value);
+void * tern_find_ptr_default(tern_node * head, char const * key, void * def);
+void * tern_find_ptr(tern_node * head, char const * key);
+tern_val tern_find_path_default(tern_node *head, char const *key, tern_val def);
+tern_val tern_find_path(tern_node *head, char const *key);
+tern_node * tern_insert_ptr(tern_node * head, char const * key, void * value);
+tern_node * tern_insert_node(tern_node *head, char const *key, tern_node *value);
 uint32_t tern_count(tern_node *head);
 void tern_foreach(tern_node *head, iter_fun fun, void *data);
 char * tern_int_key(uint32_t key, char * buf);
