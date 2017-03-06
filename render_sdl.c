69673783c8539e95e3ce9acd304b47a126c7e74f
Michael Pavone <pavone@retrodev.com>
2017-03-06 09:46:50 -0800

    Width on glTexSubImage2D call in render_framebuffer_updated needs to match the buffer pitch

--- render_sdl.c
+++ render_sdl.c
@@ -575,7 +575,7 @@ void render_framebuffer_updated(uint8_t which, int width)
 #ifndef DISABLE_OPENGL
 	if (render_gl && which <= FRAMEBUFFER_EVEN) {
 		glBindTexture(GL_TEXTURE_2D, textures[which]);
-		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_BGRA, GL_UNSIGNED_BYTE, texture_buf + overscan_left[video_standard] + LINEBUF_SIZE * overscan_top[video_standard]);
+		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, LINEBUF_SIZE, height, GL_BGRA, GL_UNSIGNED_BYTE, texture_buf + overscan_left[video_standard] + LINEBUF_SIZE * overscan_top[video_standard]);
 
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
