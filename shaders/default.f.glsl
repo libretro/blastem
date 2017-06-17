1ff6560418af2356894545dc6565a6b0bef4fd10
Michael Pavone <pavone@retrodev.com>
2017-06-17 15:43:44 -0700

    Slightly less broken default fragment shader. Looks much better with nearest neighbor, still needs work for linear interpolation

--- shaders/default.f.glsl
+++ shaders/default.f.glsl
@@ -8,7 +8,7 @@ void main()
 {
 	gl_FragColor = mix(
 		texture2D(textures[0], texcoord),
-		texture2D(textures[1], vec2(texcoord.x, texcoord.y - 1.0/1024.0)),
-		sin((texcoord.y * 1024.0 - 0.75) * 3.14159265359) / 2.0 + 0.5
+		texture2D(textures[1], vec2(texcoord.x, texcoord.y - 0.5/1024.0)),
+		(sin((texcoord.y * 1024.0 + 1.0)* 3.14159265359) + 1.0) * 0.5
 	);
 }
