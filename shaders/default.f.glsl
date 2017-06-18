91ffae8528e912485f2dcfa1464358d2b87b87e0
Michael Pavone <pavone@retrodev.com>
2017-06-17 17:43:47 -0700

    Minor simplification of default fragment shader

--- shaders/default.f.glsl
+++ shaders/default.f.glsl
@@ -8,8 +8,8 @@ void main()
 {
 	vec2 modifiedCoord = vec2(texcoord.x, (floor(texcoord.y * 512.0) + 0.5)/512.0);
 	gl_FragColor = mix(
-		texture2D(textures[0], modifiedCoord),
 		texture2D(textures[1], modifiedCoord),
-		(sin((texcoord.y * 1024.0 + 1.0)* 3.14159265359) + 1.0) * 0.5
+		texture2D(textures[0], modifiedCoord),
+		(sin(texcoord.y * 1024.0 * 3.14159265359) + 1.0) * 0.5
 	);
 }
