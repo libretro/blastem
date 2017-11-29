0b409a1a9ccc9d7d566a0a867eec4f6eb5a1f5e1
Michael Pavone <pavone@retrodev.com>
2017-11-29 08:41:37 -0800

    Added code to persist config back to a file
    --HG--
    branch : nuklear_ui

--- paths.h
+++ paths.h
@@ -2,6 +2,6 @@
 #define PATHS_H_
 
 void get_initial_browse_path(char **dst);
-char *path_append(char *base, char *suffix);
+char *path_append(const char *base, const char *suffix);
 
 #endif //PATHS_H_
