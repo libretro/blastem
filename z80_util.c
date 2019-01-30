3673c36abff5d5cd4c9e5cb8eaca88dd177b28b4
Michael Pavone <pavone@retrodev.com>
2019-01-29 23:56:48 -0800

    Implement 16-bit addition in new Z80 core along with necessary CPU DSL fixes to make them work right

--- z80_util.c
+++ z80_util.c
@@ -1,11 +1,13 @@
 
 void z80_read_8(z80_context *context)
 {
+	context->cycles += 3 * context->opts->gen.clock_divider;
 	context->scratch1 = read_byte(context->scratch1, NULL, &context->opts->gen, context);
 }
 
 void z80_write_8(z80_context *context)
 {
+	context->cycles += 3 * context->opts->gen.clock_divider;
 	write_byte(context->scratch2, context->scratch1, NULL, &context->opts->gen, context);
 }
 
