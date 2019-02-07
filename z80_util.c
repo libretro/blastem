78ee2548ecee9b30dbfc359b95c845ae1a47e12b
Michael Pavone <pavone@retrodev.com>
2019-02-07 09:43:25 -0800

    Added init functions to z80_util.c so new Z80 core is closer to a drop in replacement for the old one

--- z80_util.c
+++ z80_util.c
@@ -1,3 +1,4 @@
+#include <string.h>
 
 void z80_read_8(z80_context *context)
 {
@@ -44,3 +45,30 @@ void z80_io_write8(z80_context *context)
 	context->opts->gen.memmap = tmp_map;
 	context->opts->gen.memmap_chunks = tmp_chunks;
 }
+
+//quick hack until I get a chance to change which init method these get passed to
+static memmap_chunk const * tmp_io_chunks;
+static uint32_t tmp_num_io_chunks, tmp_io_mask;
+void init_z80_opts(z80_options * options, memmap_chunk const * chunks, uint32_t num_chunks, memmap_chunk const * io_chunks, uint32_t num_io_chunks, uint32_t clock_divider, uint32_t io_address_mask)
+{
+	memset(options, 0, sizeof(*options));
+	options->gen.memmap = chunks;
+	options->gen.memmap_chunks = num_chunks;
+	options->gen.address_mask = 0xFFFF;
+	options->gen.max_address = 0xFFFF;
+	options->gen.clock_divider = clock_divider;
+	tmp_io_chunks = io_chunks;
+	tmp_num_io_chunks = num_io_chunks;
+	tmp_io_mask = io_address_mask;
+}
+
+z80_context * init_z80_context(z80_options *options)
+{
+	z80_context *context = calloc(1, sizeof(z80_context));
+	context->opts = options;
+	context->io_map = (memmap_chunk *)tmp_io_chunks;
+	context->io_chunks = tmp_num_io_chunks;
+	context->io_mask = tmp_io_mask;
+	return context;
+}
+
